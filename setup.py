a85395bee65bf889c7476088d41917493e1128d9
Roland van Laar <roland@micite.net>
2014-07-15 07:01:50 +0200

    Update setup.py to reflect README name change.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '5.5dev'
 setup(name='v2.theme',
       version=version,
       description="An installable theme for Plone 4",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
