32557be8434a2f93d538612b51b6c0794dd7cb95
Satoshi Shiraishi <sachoco@gmail.com>
2015-01-14 22:50:16 +0100

    Design update (menu, home page)

--- v2/theme/browser/javascripts/v2.js
+++ v2/theme/browser/javascripts/v2.js
@@ -30,7 +30,7 @@ var v2 = function() {
 
             // $("body").css("background-color","#ccc");
             var s = Snap(document.getElementById('v2-logo'));
-            var scaleValue = Math.random()/2 + 1.3;
+            var scaleValue = Math.random()/2 + 1.2;
             var rotateValue = (Math.random() - 0.5) * 90;
             var skewValue = (Math.random() - 0.5) * 60;
             var myMatrix = new Snap.Matrix();
@@ -40,13 +40,15 @@ var v2 = function() {
             myMatrix.rotate(rotateValue, 228, 59);        // rotate
             // var c = s.rect(0, 0, 455, 118);
             // c.attr({fill: "#cccccc"});
-            var colorArr = ["#000000","#FFFFFF","#2BFF3A","#32FF9C","#FFFF2D","#B373FF","#00FFFF","#FF434E","#5FBEFF"];
+            // var colorArr = ["#000000","#FFFFFF","#2BFF3A","#32FF9C","#FFFF2D","#B373FF","#00FFFF","#FF434E","#5FBEFF"];
+            var colorArr = ["#000000"];
             var randColor = colorArr[Math.floor(Math.random() * colorArr.length)];
 
             Snap.load("../++resource++v2.theme.images/v2_logo.svg", function (f) {
                 var main = f.select("#main");
                 // main.transform("r18 0 0");
                 // main.transform(myMatrix);
+                // main.transform("translate(0, "+Math.abs(rotateValue)*scaleValue+") scale("+scaleValue+") rotate("+rotateValue+", 180, 59) skewX("+skewValue+")");
                 main.transform("scale("+scaleValue+") rotate("+rotateValue+", 180, 59) skewX("+skewValue+")");
                 main.selectAll("path").attr({
                     fill: randColor
