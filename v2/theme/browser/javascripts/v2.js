8af6d32fa78acc6fbd25630ee943563a82ea7bc1
Satoshi Shiraishi <sachoco@gmail.com>
2015-04-24 13:46:54 +0200

    Homepage and Upcoming Events update

--- v2/theme/browser/javascripts/v2.js
+++ v2/theme/browser/javascripts/v2.js
@@ -28,33 +28,27 @@ var v2 = function() {
 	            });
             }
 
-            // $("body").css("background-color","#ccc");
-            var s = Snap(document.getElementById('v2-logo'));
-            var scaleValue = Math.random()/2 + 1.2;
-            var rotateValue = (Math.random() - 0.5) * 90;
-            var skewValue = (Math.random() - 0.5) * 60;
-            var myMatrix = new Snap.Matrix();
+            // var s = Snap(document.getElementById('v2-logo'));
+            // var scaleValue = Math.random()/2 + 1.2;
+            // var rotateValue = (Math.random() - 0.5) * 90;
+            // var skewValue = (Math.random() - 0.5) * 60;
+            // var myMatrix = new Snap.Matrix();
 
-            myMatrix.scale(scaleValue,scaleValue,80,59);        // play with scaling before and after the rotate 
-            myMatrix.translate(0,0);      // this translate will not be applied to the rotation
-            myMatrix.rotate(rotateValue, 228, 59);        // rotate
-            // var c = s.rect(0, 0, 455, 118);
-            // c.attr({fill: "#cccccc"});
-            // var colorArr = ["#000000","#FFFFFF","#2BFF3A","#32FF9C","#FFFF2D","#B373FF","#00FFFF","#FF434E","#5FBEFF"];
-            var colorArr = ["#000000"];
-            var randColor = colorArr[Math.floor(Math.random() * colorArr.length)];
+            // myMatrix.scale(scaleValue,scaleValue,80,59);        // play with scaling before and after the rotate 
+            // myMatrix.translate(0,0);      // this translate will not be applied to the rotation
+            // myMatrix.rotate(rotateValue, 228, 59);        // rotate
 
-            Snap.load("../++resource++v2.theme.images/v2_logo.svg", function (f) {
-                var main = f.select("#main");
-                // main.transform("r18 0 0");
-                // main.transform(myMatrix);
-                // main.transform("translate(0, "+Math.abs(rotateValue)*scaleValue+") scale("+scaleValue+") rotate("+rotateValue+", 180, 59) skewX("+skewValue+")");
-                main.transform("scale("+scaleValue+") rotate("+rotateValue+", 180, 59) skewX("+skewValue+")");
-                main.selectAll("path").attr({
-                    fill: randColor
-                });
-                s.append(main);
-            });
+            // var colorArr = ["#000000"];
+            // var randColor = colorArr[Math.floor(Math.random() * colorArr.length)];
+
+            // Snap.load("../++resource++v2.theme.images/v2_logo.svg", function (f) {
+            //     var main = f.select("#main");
+            //     main.transform("scale("+scaleValue+") rotate("+rotateValue+", 180, 59) skewX("+skewValue+")");
+            //     main.selectAll("path").attr({
+            //         fill: randColor
+            //     });
+            //     s.append(main);
+            // });
 
 
         }
